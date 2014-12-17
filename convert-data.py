f8e4d7330a713553982495edc091d1cacbff0458
Mitar <mitar.git@tnode.com>
2014-12-17 18:16:13 +0100

    Improving plots.

--- convert-data.py
+++ convert-data.py
@@ -2,6 +2,7 @@
 
 import re
 import sys
+from math import log10
 
 current_is_read = None
 current_type = None
@@ -15,15 +16,18 @@ for line in sys.stdin:
 
   match = re.search(r'^/benchmark/jsons/(number|size)_(\d+).json ([0-9.]+)(?: ([0-9.]+))?$', line)
   if match:
+    x = float(match.group(2))
+    if match.group(1) == 'size':
+      x = log10(x)
     if match.group(4):
       assert not current_is_read
       with open('%s-%s-write.txt' % (current_type, match.group(1)), 'a') as f:
-        f.write('%d %f\n' % (int(match.group(2)), float(match.group(3))))
+        f.write('%d %f\n' % (x, float(match.group(3))))
       with open('%s-%s-converge.txt' % (current_type, match.group(1)), 'a') as f:
-        f.write('%d %f\n' % (int(match.group(2)), float(match.group(4))))   
+        f.write('%d %f\n' % (x, float(match.group(4))))
     else:
       with open('%s-%s-%s.txt' % (current_type, match.group(1), 'read' if current_is_read else 'write'), 'a') as f:
-        f.write('%d %f\n' % (int(match.group(2)), float(match.group(3))))
+        f.write('%d %f\n' % (x, float(match.group(3))))
     continue
 
   raise Exception("Invalid line: %s" % line)
